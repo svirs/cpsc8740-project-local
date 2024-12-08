cebfccf3be9799271131f21365da5fffc286d7e2
svirs <shur@sanity.io>
2024-12-08 15:10:24 -0800

    relative db path; try/except sqlite errors

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,4 @@
+import os
 import sqlite3
 import time
 import bcrypt
@@ -8,6 +9,8 @@ import job
 app = Flask(__name__)
 app.secret_key = b"Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K"  # for session
 
+db_path = os.path.join(os.path.dirname(__file__), "./db/database.db")
+
 
 # http://localhost:3000/api/user
 @app.route("/user", methods=["GET"])
@@ -15,17 +18,20 @@ def user_get():
     # validate user exists
     username = request.args.get("username")
     password = request.args.get("password")
-    with sqlite3.connect("./db/database.db") as cnx:
-        cursor = cnx.cursor()
-        cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
-        user = cursor.fetchone()
-        if not user:
-            return redirect("/login?failed=Unauthorized")
-        id, _, hashed_password = user
-        if bcrypt.checkpw(password.encode("utf-8"), hashed_password):
-            session["id"] = id
-        else:
-            return redirect("/login?failed=Unauthorized")
+    try:
+        with sqlite3.connect(db_path) as cnx:
+            cursor = cnx.cursor()
+            cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+            user = cursor.fetchone()
+            if not user:
+                return redirect("/login?failed=Unauthorized")
+            id, _, hashed_password = user
+            if bcrypt.checkpw(password.encode("utf-8"), hashed_password):
+                session["id"] = id
+            else:
+                return redirect("/login?failed=Unauthorized")
+    except:
+        return redirect("/login?failed=true")
 
     return redirect("/")
 
@@ -39,21 +45,24 @@ def user_post():
     if len(password) < 4:
         return redirect("/signup?failed=true")
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-    with sqlite3.connect("./db/database.db") as cnx:
-        cursor = cnx.cursor()
-        # check if user exists no duplicates (consider unique constraint)
-        cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
-        user = cursor.fetchone()
-        if user:
-            return redirect("/signup?failed=true")
-        cursor.execute(
-            "INSERT INTO users (username, password) VALUES (?, ?)",
-            (username, hashed_password),
-        )
-        cnx.commit()
-        cursor.execute("SELECT userId FROM users WHERE username = ?", (username,))
-        data = cursor.fetchone()
-        session["id"] = data[0]
+    try:
+        with sqlite3.connect(db_path) as cnx:
+            cursor = cnx.cursor()
+            # check if user exists no duplicates (consider unique constraint)
+            cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+            user = cursor.fetchone()
+            if user:
+                return redirect("/signup?failed=true")
+            cursor.execute(
+                "INSERT INTO users (username, password) VALUES (?, ?)",
+                (username, hashed_password),
+            )
+            cnx.commit()
+            cursor.execute("SELECT userId FROM users WHERE username = ?", (username,))
+            data = cursor.fetchone()
+            session["id"] = data[0]
+    except:
+        return redirect("/signup?failed=true")
 
     response = make_response(redirect("/"))
     return response
@@ -66,34 +75,39 @@ def data_get():
     user = session.get("id")
     if not user:
         return make_response("user not logged in", 401)
-    with sqlite3.connect("./db/database.db") as cnx:
-        cursor = cnx.cursor()
-        cursor.execute("SELECT * FROM users WHERE userId = ?", (user,))
-        data = cursor.fetchone()
-        if not data:
-            return make_response("user not found", 404)
-        cursor.execute("SELECT count(*) FROM new_ratings WHERE userId = ?", (user,))
-        data = cursor.fetchone()
-        if not data or data[0] < 5:
-            return make_response("missing ratings for recommendations", 403)
-        cursor.execute("SELECT count(*) FROM recommendations WHERE userId = ?", (user,))
-        data = cursor.fetchone()
-        if not data:
-            return make_response("recommendations not ready", 204)
-        # show reccomendations
-        cursor.execute(
-            """
-            SELECT m.* FROM recommendations r
-            JOIN movies m ON r.movieId = m.movieId
-            WHERE r.userId = ?
-            """,
-            (user,),
-        )
-        data = cursor.fetchall()
-        return [
-            {"id": id, "title": title, "genres": genres.split("|")}
-            for id, title, genres in data
-        ]
+    try:
+        with sqlite3.connect(db_path) as cnx:
+            cursor = cnx.cursor()
+            cursor.execute("SELECT * FROM users WHERE userId = ?", (user,))
+            data = cursor.fetchone()
+            if not data:
+                return make_response("user not found", 404)
+            cursor.execute("SELECT count(*) FROM new_ratings WHERE userId = ?", (user,))
+            data = cursor.fetchone()
+            if not data or data[0] < 5:
+                return make_response("missing ratings for recommendations", 403)
+            cursor.execute(
+                "SELECT count(*) FROM recommendations WHERE userId = ?", (user,)
+            )
+            data = cursor.fetchone()
+            if not data:
+                return make_response("recommendations not ready", 204)
+            # show reccomendations
+            cursor.execute(
+                """
+                SELECT m.* FROM recommendations r
+                JOIN movies m ON r.movieId = m.movieId
+                WHERE r.userId = ?
+                """,
+                (user,),
+            )
+            data = cursor.fetchall()
+            return [
+                {"id": id, "title": title, "genres": genres.split("|")}
+                for id, title, genres in data
+            ]
+    except:
+        return make_response("error", 500)
 
 
 # http://localhost:3000/api/onboarding
@@ -103,21 +117,24 @@ def onboarding_get():
     user = session.get("id")
     if not user:
         return make_response("user not logged in", 401)
-    with sqlite3.connect("./db/database.db") as cnx:
-        cursor = cnx.cursor()
-        cursor.execute("SELECT * FROM users WHERE userId = ?", (user,))
-        data = cursor.fetchone()
-        if not data:
-            return make_response("user not found", 404)
-        cursor.execute("SELECT count(*) FROM new_ratings WHERE userId = ?", (user,))
-        data = cursor.fetchone()
-        if not data:
-            return make_response("user not found", 404)
-        [ratings] = data
+    try:
+        with sqlite3.connect(db_path) as cnx:
+            cursor = cnx.cursor()
+            cursor.execute("SELECT * FROM users WHERE userId = ?", (user,))
+            data = cursor.fetchone()
+            if not data:
+                return make_response("user not found", 404)
+            cursor.execute("SELECT count(*) FROM new_ratings WHERE userId = ?", (user,))
+            data = cursor.fetchone()
+            if not data:
+                return make_response("user not found", 404)
+            [ratings] = data
 
-        if ratings >= 5:
-            return make_response("user already onboarded", 403)
-        return {"ratings": ratings}
+            if ratings >= 5:
+                return make_response("user already onboarded", 403)
+            return {"ratings": ratings}
+    except:
+        return make_response("error", 500)
 
 
 # http://localhost:3000/api/movies
@@ -130,29 +147,32 @@ def movie_get():
     title = request.args.get("title")
     if not title:
         return make_response("movie title not provided", 400)
-    with sqlite3.connect("./db/database.db") as cnx:
-        cursor = cnx.cursor()
-        cursor.execute(
-            """
-            WITH user_ratings AS (
-                SELECT movieId from new_ratings WHERE userId = ?
+    try:
+        with sqlite3.connect(db_path) as cnx:
+            cursor = cnx.cursor()
+            cursor.execute(
+                """
+                WITH user_ratings AS (
+                    SELECT movieId from new_ratings WHERE userId = ?
+                )
+                SELECT m.* FROM movies m
+                LEFT JOIN user_ratings ur ON m.movieId = ur.movieId
+                WHERE lower(m.title) like ? and ur.movieId is null
+                limit 10""",
+                (
+                    user,
+                    f"%{title.lower()}%",
+                ),
             )
-            SELECT m.* FROM movies m
-            LEFT JOIN user_ratings ur ON m.movieId = ur.movieId
-            WHERE lower(m.title) like ? and ur.movieId is null
-            limit 10""",
-            (
-                user,
-                f"%{title.lower()}%",
-            ),
-        )
-        data = cursor.fetchall()
-        if not data:
-            return make_response("movie not found", 404)
-        return [
-            {"id": id, "title": title, "genres": genres.split("|")}
-            for id, title, genres in data
-        ]
+            data = cursor.fetchall()
+            if not data:
+                return make_response("movie not found", 404)
+            return [
+                {"id": id, "title": title, "genres": genres.split("|")}
+                for id, title, genres in data
+            ]
+    except:
+        return make_response("error", 500)
 
 
 # http://localhost:3000/api/rating
@@ -168,27 +188,34 @@ def rating_post():
     rating = request.json["rating"]
     if rating is None or not 0 <= rating <= 5:
         return make_response("invalid rating", 400)
-    with sqlite3.connect("./db/database.db") as cnx:
-        cursor = cnx.cursor()
-        # find if rating exists
-        cursor.execute(
-            "SELECT * FROM new_ratings WHERE userId = ? AND movieId = ?",
-            (user, movie_id),
-        )
-        data = cursor.fetchone()
-        if data:
-            return make_response("rating already exists", 403)
-        cursor.execute(
-            "INSERT INTO new_ratings (userId, movieId, rating, timestamp) VALUES (?, ?, ?, ?)",
-            (user, movie_id, rating, int(time.time())),
-        )
-        cnx.commit()
-        return make_response("rating added", 200)
+    try:
+        with sqlite3.connect(db_path) as cnx:
+            cursor = cnx.cursor()
+            # find if rating exists
+            cursor.execute(
+                "SELECT * FROM new_ratings WHERE userId = ? AND movieId = ?",
+                (user, movie_id),
+            )
+            data = cursor.fetchone()
+            if data:
+                return make_response("rating already exists", 403)
+            cursor.execute(
+                "INSERT INTO new_ratings (userId, movieId, rating, timestamp) VALUES (?, ?, ?, ?)",
+                (user, movie_id, rating, int(time.time())),
+            )
+            cnx.commit()
+            return make_response("rating added", 200)
+    except:
+        return make_response("error", 500)
 
-# @app.route("/logout", methods=["GET"])
 
 @app.route("/job", methods=["GET"])
 def job_get():
     # run job
     job.run()
     return make_response("job done", 200)
+
+
+@app.route("/test", methods=["GET"])
+def test_get():
+    return make_response("test", 200)
